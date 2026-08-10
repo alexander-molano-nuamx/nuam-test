4e36119f395533a81856cac4408645f2cb14a735
AlexanderMolano <81302682+AlexanderMolano@users.noreply.github.com>
2026-08-10 10:10:57 -0500

    merge: MUI_LICENSE_KEY VITE_LICENSE in .ts

--- src/muiLicense.ts
+++ src/muiLicense.ts
@@ -5,13 +5,13 @@ import { LicenseInfo } from "@mui/x-license";
  * La key se obtiene de la variable de entorno MUI_LICENSE_KEY
  */
 function initializeMuiLicense() {
-  const licenseKey = import.meta.env.MUI_LICENSE_KEY;
+  const licenseKey = import.meta.env.VITE_MUI_LICENSE;
 
   if (!licenseKey) {
     console.warn(
       "⚠️ MUI X License is deployed by DevOPs team.",
       "Please contact them to obtain the license key",
-      "and set it in the MUI_LICENSE_KEY environment variable.",
+      "and set it in the VITE_MUI_LICENSE environment variable.",
     );
     return;
   }
